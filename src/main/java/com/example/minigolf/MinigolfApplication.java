4a73ba5691511862e28b835c42429173ffbe9284
alienworld <69631697+alienworld1@users.noreply.github.com>
2026-02-26 04:14:39 +0000

    initial commit

---
+++ src/main/java/com/example/minigolf/MinigolfApplication.java
@@ -0,0 +1,13 @@
+package com.example.minigolf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinigolfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinigolfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/minigolf/MinigolfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minigolf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinigolfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
